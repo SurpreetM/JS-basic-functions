be08fc299d3c92180d22340029192a14f0ea9f12
Surpreet Mundra <surpreet.mundra@gmail.com>
2022-01-04 13:29:58 -0500

    nested scope example

--- index.js
+++ index.js
@@ -38,3 +38,23 @@ const halve = function(n) {
 
 console.log(halve(100));
 console.log(n);
+
+// Nested scope - lexical scoping
+const hummmus = function (factor) {
+    const ingredient = function (amount, unit, name) {
+        let ingredientAmount = amount * factor; 
+        if (ingredientAmount > 1) {
+            unit += "s"
+        };
+        console.log(`${ingredientAmount} ${unit} ${name}`)
+    };
+
+    ingredient(1, "can", "chickpeas");
+    ingredient(0.25, "cup", "tahini"); 
+    ingredient(0.25, "cup", "lemon juice");
+    ingredient(1, "clove", "garlic");
+    ingredient(2, "tablespoon", "olive oil");
+    ingredient(0.5, "teaspoon", "cumin")
+};
+
+hummmus(2)
